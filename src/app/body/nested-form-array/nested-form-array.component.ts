3a629ea5209ccbbe293304afbf985bd5f41980c6
Rajamanickam <rajamanickam@sparkouttech.com>
2024-05-15 18:57:48 +0530

    nested array

--- src/app/body/nested-form-array/nested-form-array.component.ts
+++ src/app/body/nested-form-array/nested-form-array.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-nested-form-array',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./nested-form-array.component.css']
 })
 export class NestedFormArrayComponent {
+  usingNestedFormArray!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.usingNestedFormArray = this.fb.group({
+      categories: this.fb.array([],Validators.required)
+    });
+  }
+
+  get categories(): FormArray {
+    return this.usingNestedFormArray.get('categories') as FormArray;
+  }
+
+  addNewCategory(): FormGroup {
+    return this.fb.group({
+      category: this.fb.array([], Validators.required)
+    });
+  }
+
+  addCategory() {
+    this.categories.push(this.addNewCategory());
+  }
+
+  removeCategory(categoryIndex: number) {
+    this.categories.removeAt(categoryIndex);
+  }
+
+  addNewItem(categoryIndex: number): FormArray {
+    return (this.categories.at(categoryIndex).get('category') as FormArray);
+  }
+
+  newItem(): FormGroup {
+    return this.fb.group({
+      item:['', Validators.required]
+    });
+  }
+
+  addItem(categoryIndex: number) {
+    this.addNewItem(categoryIndex).push(this.newItem());
+  }
+
+  removeItem(categoryIndex: number, itemIndex: number) {
+    this.addNewItem(categoryIndex).removeAt(itemIndex);
+  }
+
+  onSubmit() {
+    console.log(this.usingNestedFormArray.value);
+  }
 }
